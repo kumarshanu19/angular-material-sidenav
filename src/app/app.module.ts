e8b64d9045c21ae7bce8a9912323b64779a4c961
Kumar Shanu <49656245+kumarshanu19@users.noreply.github.com>
2021-06-05 21:16:27 +0530

    main container update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import { OpenSidebarOnSwipeDirective } from './open-sidebar-on-swipe.directive';
 import { SidebarSwipeService } from './sidebar-swipe.service';
@@ -26,7 +28,9 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatSelectModule,
     MatButtonModule,
+    MatFormFieldModule,
     MatIconModule,
     HttpClientModule
 
